4c2b1de61b59f6b796347e7c732b3d88681da101
Robert Golawski <robert.golawski@yahoo.co.uk>
2025-04-08 13:22:11 +0100

    Add fetch tracked func

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,9 +3,8 @@
 use chrono::{DateTime, Utc};
 use reqwest;
 use serde::{Deserialize, Serialize};
-use std::fmt::write;
 use std::fs;
-use std::intrinsics::needs_drop;
+use std::hash::Hash;
 use std::path::PathBuf;
 use std::{collections::HashMap, sync::Mutex};
 use tauri::http::response;
@@ -26,10 +25,15 @@ pub struct ItemData {
 pub struct ItemPriceData {
     pub high: f64,
     pub low: f64,
-    #[serde(rename = "highTime")] // Match JSON key if different from field name
+    #[serde(rename = "highTime")]
     pub high_time: DateTime<Utc>,
-    #[serde(rename = "lowTime")] // Match JSON key if different from field name
+    #[serde(rename = "lowTime")]
     pub low_time: DateTime<Utc>,
+}
+
+#[derive(Deserialize)]
+struct PriceApiResponse {
+    pub data: HashMap<u32, ItemPriceData>,
 }
 
 pub struct AppState {
@@ -37,21 +41,41 @@ pub struct AppState {
 
     all_items_cache: Mutex<HashMap<u32, ItemData>>,
 
-    live_item_data: Mutex<HashMap<u32, ItemPriceData>>,
+    last_fetch: Mutex<DateTime<Utc>>,
+
+    http_client: reqwest::Client,
 }
 
 impl AppState {
     fn new() -> Self {
+        let user_agent = format!(
+            "{}/{} (Discord: {}, GitHub Repo: {}, Comment: {})",
+            "OsrsGeTracker",
+            env!("CARGO_PKG_VERSION"),
+            "g_dev_",
+            "github.com/RobertGolawski/TauriGeTracker",
+            "This is project I'm using for learning, so please feel free to contact me in case I do stuff wrong",
+        );
+        println!("Rust: Using User-Agent: {}", user_agent);
+
+        let client = reqwest::Client::builder()
+            .user_agent(user_agent) // Set the User-Agent for the client
+            .build()
+            .expect("Failed to build reqwest client");
+
         AppState {
             tracked_ids: Mutex::new(Vec::new()),
             all_items_cache: Mutex::new(HashMap::new()),
-            live_item_data: Mutex::new(HashMap::new()),
+            last_fetch: Mutex::new(Utc::now()),
+            http_client: client,
         }
     }
 }
 
 const ITEMS_CACHE_FILENAME: &str = "items_cache.json";
 const TRACKED_IDS_FILENAME: &str = "tracked_ids.json";
+const MAPPINGS_API_URL: &str = "https://prices.runescape.wiki/api/v1/osrs/mapping";
+const FETCH_ITEM_API_URL: &str = "https://prices.runescape.wiki/api/v1/osrs/latest";
 
 #[tauri::command]
 async fn load_initial_data(
@@ -64,43 +88,34 @@ async fn load_initial_data(
         .path()
         .app_data_dir()
         .map_err(|e| format!("Failed to get application data directory: {}", e))?;
-    // .ok_or_else(|| "Failed to get application data directory".to_string())?;
 
     fs::create_dir_all(&data_dir).map_err(|e| format!("Failed to create data directory: {}", e))?;
 
     let items_path = data_dir.join(ITEMS_CACHE_FILENAME);
 
-    let mut items_map: HashMap<u32, ItemData> = HashMap::new();
+    let items_map: HashMap<u32, ItemData>;
 
     let mut items_vec: Vec<ItemData>;
     let mut needs_fetch = false;
 
     match std::fs::read_to_string(&items_path) {
-        Ok(content) => {
-            match serde_json::from_str::<Vec<ItemData>>(&content) {
-                // Ok(items_vec) => {
-                //     println!("Rust: Deserialized {} items from file.", items_vec.len());
-                //     items_map = items_vec.into_iter().map(|item| (item.id, item)).collect();
-                //     *state.all_items_cache.lock().unwrap() = items_map.clone();
-                //     println!("Rust: Successfully loaded items cache into HashMap.");
-                // }
-                Ok(parsed_items) => {
-                    items_vec = parsed_items;
-                    println!("Rust: Successfully parsed items");
-                }
-                Err(e) => {
-                    eprintln!(
-                        "Warning: Failed to parse items cache file, will refetch: {}",
-                        e
-                    );
-
-                    items_vec = Vec::new();
-                    needs_fetch = true;
-                }
+        Ok(content) => match serde_json::from_str::<Vec<ItemData>>(&content) {
+            Ok(parsed_items) => {
+                items_vec = parsed_items;
+                println!("Rust: Successfully parsed items");
             }
-        }
+            Err(e) => {
+                eprintln!(
+                    "Warning: Failed to parse items cache file, will refetch: {}",
+                    e
+                );
+
+                items_vec = Vec::new();
+                needs_fetch = true;
+            }
+        },
         Err(e) => {
-            if e.kind() != std::io::ErrorKind::NotFound {
+            if e.kind() == std::io::ErrorKind::NotFound {
                 needs_fetch = true;
                 items_vec = Vec::new();
             } else {
@@ -109,22 +124,11 @@ async fn load_initial_data(
         }
     }
     if needs_fetch {
-        let user_agent = format!(
-            "{}/{} ({})",
-            "OsrsGeTracker",                            // Your app name
-            env!("CARGO_PKG_VERSION"),                  // Get version from Cargo.toml
-            "github.com/RobertGolawski/TauriGeTracker"  // Optional: contact/repo URL
-        );
-        println!("Rust: Using User-Agent: {}", user_agent);
-
-        let client = reqwest::Client::builder()
-            .user_agent(user_agent) // Set the User-Agent for the client
-            .build()
-            .map_err(|e| format!("Failed to build HTTP client: {}", e))?;
         println!("Rust: File not found. Fetching data and writing file.");
-        let api_url = "https://prices.runescape.wiki/api/v1/osrs/mapping";
+        let api_url = MAPPINGS_API_URL;
 
-        let response = client
+        let response = state
+            .http_client
             .get(api_url)
             .send()
             .await
@@ -142,22 +146,16 @@ async fn load_initial_data(
             .await
             .map_err(|e| format!("Failed to read body as text {}", e))?;
 
-        println!("---- API RESP BODY----");
-        // println!("{}", body_text);
-        let max_len = 1000;
-        println!("{}", body_text.chars().take(max_len).collect::<String>());
-        if body_text.len() > max_len {
-            println!("... (truncated)");
-        }
-        println!("---- API END BODY ----");
+        // println!("---- API RESP BODY----");
+        // let max_len = 1000;
+        // println!("{}", body_text.chars().take(max_len).collect::<String>());
+        // if body_text.len() > max_len {
+        //     println!("... (truncated)");
+        // }
+        // println!("---- API END BODY ----");
 
         let fetched_items_vec = serde_json::from_str::<Vec<ItemData>>(&body_text)
             .map_err(|e| format!("Failed to parse text to json: {}", e))?;
-        // let items_vec = response
-        //     .json::<Vec<ItemData>>()
-        //     .await
-        //     .map_err(|e| format!("Failed to parse JSON response from API: {}", e))?;
-        //
         println!("Rust: Successfully fetched and parsed items from API.");
 
         let json_string = serde_json::to_string_pretty(&fetched_items_vec)
@@ -169,8 +167,7 @@ async fn load_initial_data(
         items_vec = fetched_items_vec;
     }
 
-    let items_map: HashMap<u32, ItemData> =
-        items_vec.into_iter().map(|item| (item.id, item)).collect();
+    items_map = items_vec.into_iter().map(|item| (item.id, item)).collect();
     *state.all_items_cache.lock().unwrap() = items_map.clone();
     println!(
         "Rust: Updated AppState with {} items.",
@@ -213,6 +210,97 @@ async fn load_initial_data(
         initial_tracked_data.len()
     );
     Ok(initial_tracked_data)
+}
+
+async fn fetch_tracked_prices(
+    state: tauri::State<'_, AppState>,
+) -> Result<HashMap<u32, ItemPriceData>, String> {
+    let tracked_ids_guard = state.tracked_ids.lock().unwrap();
+
+    if tracked_ids_guard.is_empty() {
+        println!("No tracked items to fetch, returning empty");
+        return Ok(HashMap::new());
+    }
+
+    let ids_to_fetch = tracked_ids_guard.clone();
+
+    drop(tracked_ids_guard);
+
+    let mut fetched_prices: HashMap<u32, ItemPriceData> = HashMap::new();
+
+    let response = state
+        .http_client
+        .get(FETCH_ITEM_API_URL)
+        .send()
+        .await
+        .map_err(|e| {
+            format!(
+                "Failed to send request to {}, got {}",
+                FETCH_ITEM_API_URL, e
+            )
+        })?;
+
+    if !response.status().is_success() {
+        return Err(format!(
+            "API request failed with status: {}",
+            response.status()
+        ));
+    }
+
+    let body_text = response
+        .text()
+        .await
+        .map_err(|e| format!("Failed to read the latest fetch map {}", e))?;
+
+    let full_price_data = serde_json::from_str::<PriceApiResponse>(&body_text)
+        .map_err(|e| format!("Failed to parse price data from JSON {}", e))?;
+
+    // for item_id in &ids_to_fetch {
+    //     if full_price_data.data.contains_key(&item_id) {
+    //         fetched_prices.insert(&item_id, full_price_data.data.get(&item_id));
+    //     }
+    // }
+    for item_id in &ids_to_fetch {
+        if let Some(price_data_ref) = full_price_data.data.get(item_id) {
+            fetched_prices.insert(*item_id, price_data_ref.clone());
+        } else {
+            eprintln!(
+                "Warning: Price data for tracked item ID {} not found",
+                item_id
+            );
+        }
+    }
+
+    // for item in &ids_to_fetch {
+    //     let fetch_url = format!("{}?id={}", FETCH_ITEM_API_URL, item);
+    //     let response = state
+    //         .http_client
+    //         .get(&fetch_url)
+    //         .send()
+    //         .await
+    //         .map_err(|e| format!("Failed to send request to API: {}", e))?;
+    //
+    //     if !response.status().is_success() {
+    //         return Err(format!(
+    //             "API request failed with status: {}",
+    //             response.status()
+    //         ));
+    //     }
+    //
+    //     let body_text = response
+    //         .text()
+    //         .await
+    //         .map_err(|e| format!("Failed to read body as text {}", e))?;
+    //
+    //     println!("{}", body_text);
+    //     let fetched_item_data = serde_json::from_str::<ItemPriceData>(&body_text)
+    //         .map_err(|e| format!("Failed to parse text to json: {}", e))?;
+    //     println!("Rust: Successfully fetched and parsed items from API.");
+    //
+    //     fetched_prices.insert(*item, fetched_item_data);
+    // }
+
+    Ok(fetched_prices)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
